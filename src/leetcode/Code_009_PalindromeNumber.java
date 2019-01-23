16885823da8f8493de2515c70c9d32dfeff3633f
xuliang <xuliang@qiushibaike.com>
2019-01-23 14:12:06 +0800

    feat: 009 回文数

---
+++ src/leetcode/Code_009_PalindromeNumber.java
@@ -0,0 +1,63 @@
+package leetcode;
+
+/**
+ * Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 121
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ * Example 3:
+ * <p>
+ * Input: 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ * Follow up:
+ * <p>
+ * Coud you solve it without converting the integer to a string?
+ * --------------------------------------------------------------------------
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 121
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ * 示例 3:
+ * <p>
+ * 输入: 10
+ * 输出: false
+ * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+ * 进阶:
+ * <p>
+ * 你能不将整数转为字符串来解决这个问题吗？
+ */
+public class Code_009_PalindromeNumber {
+
+    // Runtime: 102 ms, faster than 69.39% of Java online submissions for Palindrome Number.
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int temp = x;
+        long result = 0;
+        while (x != 0) {
+            result = 10 * result + x % 10;
+            x /= 10;
+        }
+        return temp == result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Code_009_PalindromeNumber().isPalindrome(19291));
+    }
+}
