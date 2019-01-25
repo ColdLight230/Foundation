fbbf688dc624e25191168abd831eeea4a99c4a51
xuliang <xuliang@qiushibaike.com>
2019-01-25 10:26:22 +0800

    feat: 121 买卖股票最佳时机

---
+++ src/leetcode/Code_121_BestTimeToBuyAndSellStock.java
@@ -0,0 +1,75 @@
+package leetcode;
+
+/**
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * <p>
+ * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+ * <p>
+ * Note that you cannot sell a stock before you buy one.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+ * Not 7-1 = 6, as selling price needs to be larger than buying price.
+ * Example 2:
+ * <p>
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ * ----------------------------------------------------------------------------------------
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * <p>
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+ * <p>
+ * 注意你不能在买入股票前卖出股票。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 5
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ * 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+ * 示例 2:
+ * <p>
+ * 输入: [7,6,4,3,1]
+ * 输出: 0
+ * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ */
+public class Code_121_BestTimeToBuyAndSellStock {
+
+    // 执行用时: 550 ms, 在Best Time to Buy and Sell Stock的Java提交中击败了5.42% 的用户
+    public int maxProfit1(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] > prices[i] && prices[j] - prices[i] > maxProfit) {
+                    maxProfit = prices[j] - prices[i];
+                }
+            }
+        }
+        return maxProfit;
+    }
+
+    // 执行用时: 2 ms, 在Best Time to Buy and Sell Stock的Java提交中击败了87.10% 的用户
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+
+
+    public static void main(String[] args) {
+//        int[] nums = new int[]{7,1,5,3,6,4};
+        int[] nums = new int[]{7, 6, 4, 3, 1};
+        System.out.println(new Code_121_BestTimeToBuyAndSellStock().maxProfit(nums));
+    }
+}
