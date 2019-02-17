12d17e91adc9d8b825e287a5c91a254015909643
xuliang <xuliang@qiushibaike.com>
2019-02-17 21:37:02 +0800

    feat: 108 将有序数组转换为二叉搜索树

---
+++ src/leetcode/Code_108_ConvertSortedArraytoBinarySearchTree.java
@@ -0,0 +1,72 @@
+package leetcode;
+
+/**
+ * of every node never differ by more than 1.
+ *
+ * Example:
+ *
+ * Given the sorted array: [-10,-3,0,5,9],
+ *
+ * One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+ *
+ *       0
+ *      / \
+ *    -3   9
+ *    /   /
+ *  -10  5
+ *  -----------------------------------------------------------------------------
+ *  将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+ *
+ * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+ *
+ * 示例:
+ *
+ * 给定有序数组: [-10,-3,0,5,9],
+ *
+ * 一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+ *
+ *       0
+ *      / \
+ *    -3   9
+ *    /   /
+ *  -10  5
+ */
+public class Code_108_ConvertSortedArraytoBinarySearchTree {
+
+    // 执行用时: 1 ms, 在Convert Sorted Array to Binary Search Tree的Java提交中击败了97.11% 的用户
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        return buildTree(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildTree(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        if (left == right) return new TreeNode(nums[left]);
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = buildTree(nums, left, mid - 1);
+        root.right = buildTree(nums, mid + 1, right);
+        return root;
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
