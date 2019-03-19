15fa8188c14b664368ddea9871d1651fe1a77ae6
xuliang <xuliang@qiushibaike.com>
2019-03-19 09:43:10 +0800

    feat: 053 最大子序和

---
+++ src/leetcode/Code_053_MaximumSubArray.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+/**
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+ * <p>
+ * Example:
+ * <p>
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * Follow up:
+ * <p>
+ * If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+ * -----------------------------------------------------------------------------
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [-2,1,-3,4,-1,2,1,-5,4],
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ * 进阶:
+ * <p>
+ * 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+ */
+public class Code_053_MaximumSubArray {
+
+    // 执行用时 : 15 ms, 在Maximum Subarray的Java提交中击败了73.97% 的用户
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int sum = 0;
+        for (int num : nums) {
+            if (sum>0){
+                sum += num;
+            }else {
+                sum = num;
+            }
+            max = Math.max(sum,max);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(new Code_053_MaximumSubArray().maxSubArray(nums));
+    }
+}
