ed08304e379420c3697ef11c05b3d5f92953d305
xuliang <xuliang@qiushibaike.com>
2019-08-03 14:22:21 +0800

    feat: 234 回文链表

---
+++ src/leetcode/Code_234_PalindromeLinkedList.java
@@ -0,0 +1,84 @@
+package leetcode;
+
+/**
+ * Given a singly linked list, determine if it is a palindrome.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 1->2
+ * Output: false
+ * Example 2:
+ * <p>
+ * Input: 1->2->2->1
+ * Output: true
+ * Follow up:
+ * Could you do it in O(n) time and O(1) space?
+ * -----------------------------------------------------------------------------
+ * 请判断一个链表是否为回文链表。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 1->2
+ * 输出: false
+ * 示例 2:
+ * <p>
+ * 输入: 1->2->2->1
+ * 输出: true
+ * 进阶：
+ * 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ */
+public class Code_234_PalindromeLinkedList {
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        fast = slow.next;
+        slow.next = null;
+        ListNode temp;
+        while (fast != null) {
+            temp = fast.next;
+            fast.next = slow;
+            slow = fast;
+            fast = temp;
+        }
+        temp = slow;
+        fast = head;
+        boolean res = true;
+        while (slow != null && fast != null) {
+            if (slow.val != fast.val) {
+                res = false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow = temp.next;
+        temp.next = null;
+        while (slow != null) {
+            fast = slow.next;
+            slow.next = temp;
+            temp = slow;
+            slow = fast;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
