e5277fb32f6492fc89097df27004fbfc44f59f38
xuliang <xuliang@qiushibaike.com>
2019-08-15 10:18:31 +0800

    feat: 111 二叉树的最小深度

---
+++ src/leetcode/Code_111_MinimumDepthOfBinaryTree.java
@@ -0,0 +1,94 @@
+package leetcode;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given a binary tree, find its minimum depth.
+ *
+ * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+ *
+ * Note: A leaf is a node with no children.
+ *
+ * Example:
+ *
+ * Given binary tree [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * return its minimum depth = 2.
+ * -----------------------------------------------------------------------------
+ * 给定一个二叉树，找出其最小深度。
+ *
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例:
+ *
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最小深度  2.
+ */
+public class Code_111_MinimumDepthOfBinaryTree {
+
+    /**
+     * 执行用时 : 2 ms , 在所有 Java 提交中击败了 31.84% 的用户
+     * 内存消耗 : 38.1 MB , 在所有 Java 提交中击败了 49.26% 的用户
+     */
+    public int minDepth1(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            depth++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                if (poll.left == null && poll.right == null) return depth;
+                if (poll.left != null) {
+                    queue.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.offer(poll.right);
+                }
+            }
+        }
+        return depth;
+    }
+
+    /**
+     * 执行用时 : 1 ms , 在所有 Java 提交中击败了 99.64% 的用户
+     * 内存消耗 : 38.3 MB , 在所有 Java 提交中击败了 46.54% 的用户
+     */
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.left.left = new TreeNode(1);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        System.out.println(new Code_111_MinimumDepthOfBinaryTree().minDepth(root));
+    }
+}
