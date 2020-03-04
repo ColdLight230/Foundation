26a4bb763a14cd34f25ab6f1b67a7eac766bdb39
xuliang <xuliang@qiushibaike.com>
2020-03-04 10:07:35 +0800

    feat:leetcode 080 删除排序数组中的重复项II

---
+++ src/leetcode/Code_080_RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,96 @@
+package leetcode;
+
+/**
+ * Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
+ *
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ *
+ * Example 1:
+ *
+ * Given nums = [1,1,1,2,2,3],
+ *
+ * Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+ *
+ * It doesn't matter what you leave beyond the returned length.
+ * Example 2:
+ *
+ * Given nums = [0,0,1,1,1,1,2,3,3],
+ *
+ * Your function should return length = 7, with the first seven elements of nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
+ *
+ * It doesn't matter what values are set beyond the returned length.
+ * Clarification:
+ *
+ * Confused why the returned value is an integer but your answer is an array?
+ *
+ * Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+ *
+ * Internally you can think of this:
+ *
+ * // nums is passed in by reference. (i.e., without making a copy)
+ * int len = removeDuplicates(nums);
+ *
+ * // any modification to nums in your function would be known by the caller.
+ * // using the length returned by your function, it prints the first len elements.
+ * for (int i = 0; i < len; i++) {
+ *     print(nums[i]);
+ * }
+ * ----------------------------------------------------------------------------------------
+ * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 示例 1:
+ *
+ * 给定 nums = [1,1,1,2,2,3],
+ *
+ * 函数应返回新长度 length = 5, 并且原数组的前五个元素被修改为 1, 1, 2, 2, 3 。
+ *
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 示例 2:
+ *
+ * 给定 nums = [0,0,1,1,1,1,2,3,3],
+ *
+ * 函数应返回新长度 length = 7, 并且原数组的前五个元素被修改为 0, 0, 1, 1, 2, 3, 3 。
+ *
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 说明:
+ *
+ * 为什么返回数值是整数，但输出的答案是数组呢?
+ *
+ * 请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+ *
+ * 你可以想象内部操作如下:
+ *
+ * // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+ * int len = removeDuplicates(nums);
+ *
+ * // 在函数里修改输入数组对于调用者是可见的。
+ * // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+ * for (int i = 0; i < len; i++) {
+ *     print(nums[i]);
+ * }
+ */
+public class Code_080_RemoveDuplicatesFromSortedArrayII {
+
+    /**
+     * 执行用时 : 1 ms , 在所有 Java 提交中击败了 99.64% 的用户
+     * 内存消耗 : 41.3 MB , 在所有 Java 提交中击败了 5.05% 的用户
+     */
+    public int removeDuplicates(int[] nums) {
+        int slow = 1;
+        for (int fast = 2; fast < nums.length; fast++) {
+            if (nums[fast] != nums[slow - 1]) {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+        }
+        return slow + 1;
+    }
+
+    public static void main(String[] args) {
+        //        System.out.println(new Code_080_RemoveDuplicatesFromSortedArrayII().removeDuplicates(new int[]{1, 1, 1, 2, 2, 3}));
+        System.out.println(new Code_080_RemoveDuplicatesFromSortedArrayII().removeDuplicates(new int[]{0, 0, 1, 1, 1, 1, 2, 3, 3}));
+        System.out.println(new Code_080_RemoveDuplicatesFromSortedArrayII().removeDuplicates(new int[]{1, 1, 1, 2, 2, 3}));
+    }
+}
